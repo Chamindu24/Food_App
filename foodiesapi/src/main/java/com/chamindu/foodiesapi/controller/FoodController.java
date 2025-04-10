9f74730a168bbea13ff1873981da5ae4b15c5777
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-04-10 23:57:34 +0530

    add CRUD operations for food items and implement file deletion from Cloudinary

--- foodiesapi/src/main/java/com/chamindu/foodiesapi/controller/FoodController.java
+++ foodiesapi/src/main/java/com/chamindu/foodiesapi/controller/FoodController.java
@@ -8,16 +8,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/foods")
 @AllArgsConstructor
+@CrossOrigin("*")
 public class FoodController {
 
     private final FoodService foodService;
@@ -34,6 +34,19 @@ public class FoodController {
         FoodResponse foodResponse = foodService.addFood(foodRequest, file);
         return foodResponse;
 
+    }
+    @GetMapping
+    public List<FoodResponse> readFoods() {
+        return foodService.readFoods();
+    }
+    @GetMapping("/{id}")
+    public FoodResponse readFood(@PathVariable String id) {
+        return foodService.readFood(id);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFood(@PathVariable String id) {
+        foodService.deleteFood(id);
     }
 
 }
